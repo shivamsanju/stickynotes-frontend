3dcc9873d988fa17c4aa84b984afa8007844482c
Shivam Sanju <shvmsanju@gmail.com>
2022-04-16 21:21:36 +0530

    create and setup ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import DUMMY_NOTES from './DUMMY_NOTES';
+import Note from './components/Note/Note';
 
 function App() {
+  const [notesList, setNotesList] = useState<any[]>([]);
+  useEffect(() => {
+    setNotesList(DUMMY_NOTES);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="notes-list">
+        {notesList.map((note, index) => {
+          return <Note note={note} key={index} />;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/DUMMY_NOTES.ts
@@ -0,0 +1,15 @@
+import INote from './interfaces/note.interface';
+const DUMMY_NOTES: INote[] = [
+  { text: 'Learn DSA', link: 'https://www.dsa.com' },
+  { text: 'Study DBMS', link: 'https://www.dsa1.com' },
+  { text: 'Go to grocerry shop', link: 'https://www.d3sa.com' },
+  { text: 'Read a book', link: 'https://www.ds42rra.com' },
+  { text: 'Sleep Early', link: 'https://www.dsa.com' },
+  { text: 'Do Whatever you want', link: 'https://www.dsa.com' },
+  { text: 'Plant a tree', link: 'https://www.dsa.com' },
+  { text: 'Do Yoga', link: 'https://www.dsa.com' },
+  { text: 'Learn Sodtware Development', link: 'https://www.dsa.com' },
+  { text: 'Create a MERN project', link: 'https://www.dsa.com' },
+];
+
+export default DUMMY_NOTES;
